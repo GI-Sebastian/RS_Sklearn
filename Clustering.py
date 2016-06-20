3e58dd179727a25598348d56e61112b0aed8d5c8
Sebastian <sebastianscheckel@gmail.com>
2016-06-20 19:16:15 +0200

    Changes Path to Landsat scene

---
+++ Clustering.py
@@ -0,0 +1,14 @@
+import os
+
+
+
+raster_path = os.path.join(os.sep,
+                           "home",
+                           "sebastian",
+                           "Documents",
+                           "Data",
+                           "exemplary_data",
+                           "LC819202820160604")
+
+print(raster_path)
+
