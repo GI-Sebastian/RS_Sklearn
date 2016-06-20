630b7340e3869855899195275c3404d4d2e7da97
Sebastian <sebastianscheckel@gmail.com>
2016-06-20 19:38:50 +0200

    Read the L8 band 2-7, read the raster as array, and store them into a list

--- Clustering.py
+++ Clustering.py
@@ -1,7 +1,8 @@
+import glob
 import os
+import rasterio as rio
 
-
-
+# create path to the location of the Landsat GeoTiff files
 raster_path = os.path.join(os.sep,
                            "home",
                            "sebastian",
@@ -10,5 +11,18 @@ raster_path = os.path.join(os.sep,
                            "exemplary_data",
                            "LC819202820160604")
 
-print(raster_path)
+# iterate over all files matching this Unix style pattern
+raster_list = []
+for name in glob.glob(raster_path + "/*B[2-7].TIF"):
+    raster_list.append(name)
 
+# sort the list
+raster_list = sorted(raster_list)
+
+band_list = []
+for raster in raster_list:
+    with rio.open(raster) as src:
+        array = src.read()
+        band_list.append(array)
+
+print band_list
